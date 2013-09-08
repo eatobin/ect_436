b6e5b22a23af21a90ebecad2b9b494552ef0fce0
Eric Tobin <eatobin@gmail.com>
2013-09-07 22:21:32 -0500

    shortened title and wrote test. added title helper.

--- spec/requests/ones_spec.rb
+++ spec/requests/ones_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe "Homework 1" do
 
-  let(:base_title) { "Eric Tobin - ECT 436 - Assignment #" }
+  let(:base_title) { "EAT_ECT 436" }
 
   describe "Home page" do
 
-    it "should have the title 'Eric Tobin - ECT 436 - Assignment #1'" do
+    it "should have the title 'EAT_ECT 436'" do
       visit '/one/home'
-      expect(page).to have_title("#{base_title}1")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the title 'EAT_ECT 436 | 1'" do
+      visit '/one/home'
+      expect(page).not_to have_title('| 1')
     end
 
     it "should have the content 'Welcome!'" do

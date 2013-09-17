0be683cda5d5ae76383923ea521f5c33f4068e24
Eric Tobin <eatobin@gmail.com>
2013-09-17 17:33:59 -0500

    improved rspec tests using shared examples

--- spec/requests/ones_spec.rb
+++ spec/requests/ones_spec.rb
@@ -2,23 +2,21 @@ require 'spec_helper'
 
 describe "Homework 1" do
 
-  describe "Home page" do
+  subject { page }
+
+  shared_examples_for "all site pages" do
+    it { should(have_title(full_title(page_title))) }
+    it { should(have_selector('h1', {:text => heading})) }
+  end
+
+  describe "HW1 Home Page" do
     before { visit root_path }
 
-    it "should have the title 'EAT ECT 436'" do
-      expect(page).to have_selector('title', {:text => full_title(''), :visible => false})
-    end
+    let(:page_title) { '' }
+    let(:heading) { 'Welcome!' }
 
-    it "should not have the title 'EAT ECT 436 | 1'" do
-      expect(page).not_to have_selector('title', {:text => full_title(' | 1'), :visible => false})
-    end
-
-    it "should have the h1 content 'Welcome!'" do
-      expect(page).to have_selector('h1', {:text => 'Welcome!'})
-    end
-
-    it "should not have a header containing 'Return Home' link" do
-      expect(page).not_to have_link('Return Home', href: root_path)
-    end
+    it_should_behave_like("all site pages")
+    it { should_not(have_title(' | 1')) }
+    it { should_not(have_link('Return Home', href: root_path)) }
   end
 end
--- spec/requests/twos_spec.rb
+++ spec/requests/twos_spec.rb
@@ -2,55 +2,45 @@ require 'spec_helper'
 
 describe "Homework 2" do
 
+  subject { page }
+
+  shared_examples_for "all site pages" do
+    it { should(have_title(full_title(page_title))) }
+    it { should(have_selector('h1', {:text => heading})) }
+  end
+
   describe "HW2 Home Page" do
     before { visit hw2_path }
 
-    it "should have the title 'EAT ECT 436 | 2'" do
-      expect(page).to have_selector('title', {:text => full_title('2'), :visible => false})
-    end
+    let(:page_title) { '2' }
+    let(:heading) { 'Who is this strange guy?:' }
 
-    it "should have the h1 content 'Who is this strange guy?:'" do
-      expect(page).to have_selector('h1', {:text => 'Who is this strange guy?:'})
-    end
-
-    it "should have a header containing 'Return Home' link" do
-      expect(page).to have_link('Return Home', href: root_path)
-    end
-
-    it "should have a text input field named ':query'" do
-      expect(page).to have_field(:query)
-    end
+    it_should_behave_like("all site pages")
+    it { should(have_link('Return Home', href: root_path)) }
+    it { should(have_field(:query)) }
   end
 
-  describe "HW 2 Page 2" do
+  describe "HW2 Page 2" do
     before do
       visit hw2_path
       fill_in(:query, {:with => 'This is a test!'})
       click_button("Send It!")
     end
 
-    it "should have the title 'EAT ECT 436 | 2 - 2'" do
-      expect(page).to have_selector('title', {:text => full_title('2 - 2'), :visible => false})
-    end
+    let(:page_title) { '2 - 2' }
+    let(:heading) { "Here's the sentence you sent from the first page:" }
 
-    it "should have a header containing 'Return Home' link" do
-      expect(page).to have_link('Return Home', href: root_path)
-    end
+    it_should_behave_like("all site pages")
 
-    it "should have the content 'This is a test!'" do
-      expect(page).to have_content('This is a test!')
-    end
+    # works!
+    # it "should have the title 'EAT ECT 436 | 2 - 2'" do
+    #   expect(page).to have_selector('title', {:text => full_title('2 - 2'), :visible => false})
+    # end
 
-    it "should have the content '[This, is, a, test!]'" do
-      expect(page).to have_content('["This", "is", "a", "test!"]')
-    end
-
-    it "should have the content '[test!, a, is, This]'" do
-      expect(page).to have_content('["test!", "a", "is", "This"]')
-    end
-
-    it "should have the content '[test!*a*is*This]'" do
-      expect(page).to have_content('test!*a*is*This')
-    end
+    it { should(have_link('Return Home', href: root_path)) }
+    it { should(have_content('This is a test!')) }
+    it { should(have_content('["This", "is", "a", "test!"]')) }
+    it { should(have_content('["test!", "a", "is", "This"]')) }
+    it { should(have_content('test!*a*is*This')) }
   end
 end

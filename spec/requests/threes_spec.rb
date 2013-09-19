1aa65d9c4142337f299711d2f0049bf49828dbee
Eric Tobin <eatobin@gmail.com>
2013-09-19 17:53:47 -0500

    hw3 progress

---
+++ spec/requests/threes_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Homework 3" do
+
+  subject { page }
+
+  shared_examples_for "all site pages" do
+    it { should(have_title(full_title(page_title))) }
+    it { should(have_selector('h1', {:text => heading})) }
+  end
+
+  describe "HW3 'Home' Page" do
+    before { visit hw3_path }
+
+    let(:page_title) { '3' }
+    let(:heading) { "I hear you're planning a little trip:" }
+
+    it_should_behave_like("all site pages")
+    it { should(have_link('Return Home', href: root_path)) }
+    it { should(have_field(:state)) }
+  end
+
+  describe "HW3 Page '2'" do
+    before do
+      visit hw3_path
+      select('Texas', {:from => :state})
+      click_button("Send It!")
+    end
+
+    let(:page_title) { '3' }
+    let(:heading) { "TX" }
+
+    it_should_behave_like("all site pages")
+    #
+    ## works!
+    ## it "should have the title 'EAT ECT 436 | 2 - 2'" do
+    ##   expect(page).to have_selector('title', {:text => full_title('2 - 2'), :visible => false})
+    ## end
+    #
+    #it { should(have_link('Return Home', href: root_path)) }
+    #it { should(have_content('This is a test!')) }
+    #it { should(have_content('["This", "is", "a", "test!"]')) }
+    #it { should(have_content('["test!", "a", "is", "This"]')) }
+    #it { should(have_content('test!*a*is*This')) }
+  end
+end

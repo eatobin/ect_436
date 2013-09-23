e8df00a6c3a5ec109a2b6c36d52df36dfe566015
Eric Tobin <eatobin@gmail.com>
2013-09-23 13:57:34 -0500

    added beef to hw2 helper

--- app/helpers/two_helper.rb
+++ app/helpers/two_helper.rb
@@ -1,16 +1,10 @@
 module TwoHelper
 
-  @forward_string = params[:query]
-
-  def forward_string do |@forward_string|
-    @forward_string
+  def forward_array(query_string)
+    query_string.split(' ')
   end
 
-  def forward_array do |@forward_string|
-    @forward_array = @forward_string.split(' ')
-  end
-
-  def reversed_array do |@forward_string|
-    @reversed_array = @forward_string.split(' ').reverse 
+  def reversed_array(an_array)
+    an_array.reverse
   end
 end

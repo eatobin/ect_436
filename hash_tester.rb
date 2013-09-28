b24cd904358487154d345a1fb9467473e0ce3191
Eric Tobin <eatobin@gmail.com>
2013-09-28 08:45:01 -0500

    forgot tester

--- hash_tester.rb
+++ hash_tester.rb
@@ -1,9 +1,30 @@
+# d1 = Department.create( {:deptno => 101, :dname => 'sales', :loc => 'tuc'} )
+
+# d2 = Department.create( {:deptno => 102, :dname => 'accounting', :loc => 'tuc'} )
+
 def print_it
   my_array = Department.all
   my_array.each do |h|
-    #h.each do |key, value|
-      #puts("#{key} - #{value}")
-    #end
-    p(h)
+    h.attributes.each do |key, value|
+      puts("#{key} - #{value}")
+    end
+    puts('Next Record')
   end
+end
+
+def print_it2
+  my_array = Department.all
+  my_array.each do |h|
+      puts("#{h.deptno} - #{h.dname}")
+  end
+  puts()
+end
+
+def make_dd
+  dd = Array.new
+  all_depts = Department.all
+  all_depts.each do |d|
+    dd << d.dname
+  end
+  p(dd)
 end

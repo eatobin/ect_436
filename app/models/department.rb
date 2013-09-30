565561d852e9e9d17ef9e21cdab506f8f61c7c5f
Eric Tobin <eatobin@gmail.com>
2013-09-30 09:38:50 -0500

    second try at dept_id

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,5 @@
 # and http://net.tutsplus.com/tutorials/ruby/active-record-the-rails-database-bridge/
 
 class Department < ActiveRecord::Base
+  has_many :employees
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,18 @@ connection.execute("INSERT INTO departments (DEPTNO, DNAME, LOC, CREATED_AT, UPD
 (30, 'SALES', 'CHICAGO', localtimestamp, localtimestamp),
 (40, 'OPERATIONS', 'BOSTON', localtimestamp, localtimestamp);")
 
-connection.execute("INSERT INTO employees (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO, CREATED_AT, UPDATED_AT) VALUES
-(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', '5000.00', '0.00', 10, localtimestamp, localtimestamp),
-(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', '2850.00', '0.00', 30, localtimestamp, localtimestamp),
-(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', '2450.00', '0.00', 10, localtimestamp, localtimestamp),
-(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', '2975.00', '0.00', 20, localtimestamp, localtimestamp),
-(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', '1250.00', '1400.00', 30, localtimestamp, localtimestamp),
-(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', '1600.00', '300.00', 30, localtimestamp, localtimestamp),
-(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', '1500.00', '0.00', 30, localtimestamp, localtimestamp),
-(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', '950.00', '0.00', 30, localtimestamp, localtimestamp),
-(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', '1250.00', '500.00', 40, localtimestamp, localtimestamp),
-(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', '3000.00', '0.00', 20, localtimestamp, localtimestamp),
-(7369, 'SMITH', 'CLERK', 7902, '1981-12-17', '800.00', '0.00', 20, localtimestamp, localtimestamp),
-(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', '3000.00', '0.00', 20, localtimestamp, localtimestamp),
-(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', '1100.00', '0.00', 20, localtimestamp, localtimestamp),
-(7934, 'MILLER', 'CLERK', 7782, '1982-01-22', '1300.00', '0.00', 10, localtimestamp, localtimestamp);")
+connection.execute("INSERT INTO employees (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO, DEPARTMENT_ID, CREATED_AT, UPDATED_AT) VALUES
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', '5000.00', '0.00', 10, 1, localtimestamp, localtimestamp),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', '2850.00', '0.00', 30, 3, localtimestamp, localtimestamp),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', '2450.00', '0.00', 10, 1, localtimestamp, localtimestamp),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', '2975.00', '0.00', 20, 2, localtimestamp, localtimestamp),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', '1250.00', '1400.00', 30, 3, localtimestamp, localtimestamp),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', '1600.00', '300.00', 30, 3, localtimestamp, localtimestamp),
+(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', '1500.00', '0.00', 30, 3, localtimestamp, localtimestamp),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', '950.00', '0.00', 30, 3, localtimestamp, localtimestamp),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', '1250.00', '500.00', 40, 4, localtimestamp, localtimestamp),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', '3000.00', '0.00', 20, 2, localtimestamp, localtimestamp),
+(7369, 'SMITH', 'CLERK', 7902, '1981-12-17', '800.00', '0.00', 20, 2, localtimestamp, localtimestamp),
+(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', '3000.00', '0.00', 20, 2, localtimestamp, localtimestamp),
+(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', '1100.00', '0.00', 20, 2, localtimestamp, localtimestamp),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-22', '1300.00', '0.00', 10, 1, localtimestamp, localtimestamp);")

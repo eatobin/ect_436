09335add575a7082d32eb217ffe60567f7719b20
Eric Tobin <eatobin@gmail.com>
2013-10-02 08:55:53 -0500

    working on Department tests

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe Department do
-  it "should return ACCOUNTING as first dept" do
-    Department.first.dname.should eq("ACCOUNTING")
-  end
+  describe "Department Wrapper" do
+    it "should return ACCOUNTING as first dept" do
+      Department.first.dname.should eq("ACCOUNTING")
+    end
 
-  it "should return OPERATIONS as last dept" do
-    Department.last.dname.should eq("OPERATIONS")
-  end
+    it "should return OPERATIONS as last dept" do
+      Department.last.dname.should eq("OPERATIONS")
+    end
 
-  it "should return SALES as dept => 3" do
-    Department.where({:id => 3}).first.dname.should eq("SALES")
+    it "should return SALES as dept => 3" do
+      Department.where({ :id => 3 }).first.dname.should eq("SALES")
+    end
   end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Employee do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should return TOBIN as first employee" do
+    Employee.first.ename.should eq("TOBIN")
+  end
+
+  it "should return TOBIN as last employee" do
+    Employee.last.ename.should eq("TOBIN")
+  end
+
+  it "should return TOBIN as employee => 9" do
+    Employee.where({ :id => 9 }).first.ename.should eq("TOBIN")
+  end
+end
+
+describe Employee2 do
+
+  describe "Employee2-2" do
+    it "should return TOBIN as first employee" do
+      expect(Employee.first.ename.should eq("TOBIN"))
+    end
+  end
 end

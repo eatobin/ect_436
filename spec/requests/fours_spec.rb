e5dec62cd7bf6f946bf20e21afb428a1a265c174
Eric Tobin <eatobin@gmail.com>
2013-10-08 18:23:33 -0500

    hw4 progress

--- spec/requests/fours_spec.rb
+++ spec/requests/fours_spec.rb
@@ -17,30 +17,33 @@ describe "Homework 4" do
 
     it_should_behave_like("all site pages")
     it { should(have_link('Return Home', href: root_path)) }
-
+    it { should(have_selector('td', {:text => '20'})) }
+    it { should(have_selector('td', {:text => 'ACCOUNTING'})) }
+    it { should(have_selector('td', {:text => 'BOSTON'})) }
+    it { should(have_field(:dname_dd)) }
   end
 
-  #describe "HW2 Page 2" do
-  #  before do
-  #    visit hw2_path
-  #    fill_in(:query, {:with => 'This is a test!'})
-  #    click_button("Send It!")
-  #  end
-  #
-  #  let(:page_title) { '2 - 2' }
-  #  let(:heading) { "Here's the sentence you sent from the first page:" }
-  #
-  #  it_should_behave_like("all site pages")
-  #
-  #  # works!
-  #  # it "should have the title 'EAT ECT 436 | 2 - 2'" do
-  #  #   expect(page).to have_selector('title', {:text => full_title('2 - 2'), :visible => false})
-  #  # end
-  #
-  #  it { should(have_link('Return Home', href: root_path)) }
-  #  it { should(have_content('This is a test!')) }
-  #  it { should(have_content('["This", "is", "a", "test!"]')) }
-  #  it { should(have_content('["test!", "a", "is", "This"]')) }
-  #  it { should(have_content('test!*a*is*This')) }
-  #end
+  describe "HW4 Page 4" do
+    before do
+      visit hw4_path
+      select('SALES', {:from => :dname_dd})
+      click_button("Select Department")
+    end
+    #
+    #  let(:page_title) { '2 - 2' }
+    #  let(:heading) { "Here's the sentence you sent from the first page:" }
+    #
+    #  it_should_behave_like("all site pages")
+    #
+    #  # works!
+    #  # it "should have the title 'EAT ECT 436 | 2 - 2'" do
+    #  #   expect(page).to have_selector('title', {:text => full_title('2 - 2'), :visible => false})
+    #  # end
+    #
+    #  it { should(have_link('Return Home', href: root_path)) }
+    #  it { should(have_content('This is a test!')) }
+    #  it { should(have_content('["This", "is", "a", "test!"]')) }
+    #  it { should(have_content('["test!", "a", "is", "This"]')) }
+    #  it { should(have_content('test!*a*is*This')) }
+  end
 end

7bd406ed9e4b0ea81d6ba26267f515a0ebb8131b
Eric Tobin <eatobin@gmail.com>
2013-10-16 15:21:37 -0500

    nevermelt model spec working

--- spec/models/never_melt_spec.rb
+++ spec/models/never_melt_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe NeverMelt do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Validate Cones" do
+
+    it "should return Brown Derby as first cone" do
+      NeverMelt.first.pname.should eq("Brown Derby")
+    end
+
+    it "should return Mt. Everest as last cone" do
+      NeverMelt.last.pname.should eq("Mt. Everest")
+    end
+
+    it "should return Little Red as employee :id => 4" do
+      NeverMelt.where({:id => 4}).first.pname.should eq("Little Red")
+    end
+  end
 end

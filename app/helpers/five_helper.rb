6568b1d1308b73ced157c012689f138d283fcd67
Eric Tobin <eatobin@gmail.com>
2013-10-20 21:53:37 -0500

    red text!

--- app/helpers/five_helper.rb
+++ app/helpers/five_helper.rb
@@ -1,2 +1,13 @@
 module FiveHelper
+  def scoops
+    NeverMelt.order(:scoops).distinct.pluck(:scoops)
+  end
+
+  def flavors
+    NeverMelt.order(:flavor).distinct.pluck(:flavor)
+  end
+
+  def all
+    NeverMelt.order(:id).distinct.pluck(:pname, :price)
+  end
 end

58e6135deafb9735f4f07a054d7282ffefb118d1
Eric Tobin <eatobin@gmail.com>
2013-10-29 16:34:21 -0500

    finished hw5 testing

--- spec/requests/fives_spec.rb
+++ spec/requests/fives_spec.rb
@@ -26,7 +26,7 @@ describe "Homework 5" do
     it { should(have_selector('td', {:text => 'Little Red'})) }
   end
 
-  describe "HW5 Home Page - Narrow By DD" do
+  describe "HW5 Home Page - Narrow By DD => Flavors" do
     before do
       visit hw5_path
       select('Flavors', {:from => :narrow_dd})
@@ -49,7 +49,7 @@ describe "Homework 5" do
     it { should(have_selector('td', {:text => 'Little Red'})) }
   end
 
-  describe "HW5 Home Page - By Flavor DD" do
+  describe "HW5 Home Page - By Flavor DD => Chocolate" do
     before do
       visit hw5_path
       select('Flavors', {:from => :narrow_dd})
@@ -76,7 +76,7 @@ describe "Homework 5" do
     it { should_not(have_selector('td', {:text => 'Little Red'})) }
   end
 
-  describe "HW5 Home Page - By Flavor/Scoops DD" do
+  describe "HW5 Home Page - By Flavor/Scoops DD => 3 Scoops" do
     before do
       visit hw5_path
       select('Flavors', {:from => :narrow_dd})
@@ -138,22 +138,106 @@ describe "Homework 5" do
     it { should(have_selector('td', {:text => 'Little Red'})) }
   end
 
-  #describe "HW5 Page 2" do
-  #  #before do
-  #  #  visit hw5p2_path
-  #  #  #  fill_in(:query, {:with => 'This is a test!'})
-  #  #  #  click_button("Send It!")
-  #  #end
-  #
-  #  let(:page_title) { '5 - 2' }
-  #  let(:heading) { "Here are the details for your product:" }
-  #
-  #  it_should_behave_like("all site pages")
-  #
-  #  it { should(have_link('Return Home', href: root_path)) }
-  #  #it { should(have_content('This is a test!')) }
-  #  #it { should(have_content('["This", "is", "a", "test!"]')) }
-  #  #it { should(have_content('["test!", "a", "is", "This"]')) }
-  #  #it { should(have_content('test!*a*is*This')) }
-  #end
+  describe "HW5 Home Page - Narrow By DD => Scoops" do
+    before do
+      visit hw5_path
+      select('Scoops', {:from => :narrow_dd})
+      click_button("Submit")
+    end
+
+    let(:page_title) { '5' }
+    let(:heading) { "Choose An Ice Cream Cone (use the DDL if you'd like):" }
+
+    it_should_behave_like("all site pages")
+    it { should(have_link('Return Home', href: root_path)) }
+
+    it { should(have_content('Scoops')) }
+    it { should_not(have_field(:narrow_dd)) }
+    it { should(have_field(:scoops_dd)) }
+    it { should have_button('Submit') }
+    it { should have_button('Reset') }
+    it { should(have_selector('td', {:text => 'Fudge Slide'})) }
+    it { should(have_selector('td', {:text => 'Chocozilla'})) }
+    it { should(have_selector('td', {:text => 'Little Red'})) }
+  end
+
+  describe "HW5 Home Page - By Scoops DD => 3 Scoops" do
+    before do
+      visit hw5_path
+      select('Scoops', {:from => :narrow_dd})
+      click_button("Submit")
+
+      select('3 Scoops', {:from => :scoops_dd})
+      click_button("Submit")
+    end
+
+    let(:page_title) { '5' }
+    let(:heading) { "Choose An Ice Cream Cone (use the DDL if you'd like):" }
+
+    it_should_behave_like("all site pages")
+    it { should(have_link('Return Home', href: root_path)) }
+
+    it { should(have_content('3 Scoops')) }
+    it { should_not(have_field(:narrow_dd)) }
+    it { should_not(have_field(:flavors_dd)) }
+    it { should(have_field(:flavors2_dd)) }
+    it { should have_button('Submit') }
+    it { should have_button('Reset') }
+    it { should(have_selector('td', {:text => 'Mt. Everest'})) }
+    it { should(have_selector('td', {:text => 'Chocozilla'})) }
+    it { should_not(have_selector('td', {:text => 'Little Red'})) }
+  end
+
+  describe "HW5 Home Page - By Scoops/Flavors DD => Chocolate" do
+    before do
+      visit hw5_path
+      select('Scoops', {:from => :narrow_dd})
+      click_button("Submit")
+
+      select('3 Scoops', {:from => :scoops_dd})
+      click_button("Submit")
+
+      select('Chocolate', {:from => :flavors2_dd})
+      click_button("Submit")
+    end
+
+    let(:page_title) { '5' }
+    let(:heading) { "Choose An Ice Cream Cone (use the DDL if you'd like):" }
+
+    it_should_behave_like("all site pages")
+    it { should(have_link('Return Home', href: root_path)) }
+
+    it { should(have_content('Ok... this is it! Only one choice!')) }
+    it { should_not(have_field(:narrow_dd)) }
+    it { should_not(have_field(:scoops_dd)) }
+    it { should_not(have_field(:flavors2_dd)) }
+    it { should_not have_button('Submit') }
+    it { should have_button('Reset') }
+    it { should_not(have_selector('td', {:text => 'Mt. Everest'})) }
+    it { should(have_selector('td', {:text => 'Chocozilla'})) }
+    it { should_not(have_selector('td', {:text => 'Little Red'})) }
+  end
+
+  describe "HW5 Page 2" do
+    before do
+      visit hw5_path
+      click_link('Chocozilla')
+    end
+
+    let(:page_title) { '5 - 2' }
+    let(:heading) { "Here are the details for your product:" }
+
+    it_should_behave_like("all site pages")
+
+    it { should(have_link('Return Home', href: root_path)) }
+    it { should(have_selector('li', {:text => 'Product Name: Chocozilla'})) }
+    it { should(have_selector('li',
+                              {:text => 'Description: Three softball-sized scoops of to-die-for chocolate.'})) }
+    it { should(have_selector('li', {:text => 'Flavor: Chocolate'})) }
+    it { should(have_selector('li', {:text => 'Number of Scoops: 3 Scoops'})) }
+    it { should(have_selector('li', {:text => 'Price: $6.00'})) }
+    it { should(have_selector('li', {:text => 'Stock on Hand: 20'})) }
+    it { should(have_css("img[src='/assets/choc3.gif']")) }
+    it { should(have_css("img[alt='Choc3']")) }
+  end
 end
